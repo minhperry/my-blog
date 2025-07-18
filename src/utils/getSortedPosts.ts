2ec666a1fad4991dfaae32add5a0c3b8c936ce82
Tuan Minh Phan <phan.minh@tu-dortmund.de>
2025-07-19 01:44:50 +0200

    add sorted by creation mode

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,18 +1,25 @@
 import type { CollectionEntry } from "astro:content";
 import postFilter from "./postFilter";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
-  return posts
-    .filter(postFilter)
-    .sort(
-      (a, b) =>
-        Math.floor(
-          new Date(b.data.modDatetime ?? b.data.pubDatetime).getTime() / 1000
-        ) -
-        Math.floor(
-          new Date(a.data.modDatetime ?? a.data.pubDatetime).getTime() / 1000
-        )
+type SortMode = "created" | "modified";
+
+const getSortedPosts = (
+  posts: CollectionEntry<"blog">[],
+  mode: SortMode = "created"
+) => {
+  return posts.filter(postFilter).sort((a, b) => {
+    const getDate = (post: CollectionEntry<"blog">) => {
+      if (mode === "modified") {
+        return new Date(post.data.modDatetime ?? post.data.pubDatetime);
+      }
+      return new Date(post.data.pubDatetime);
+    };
+
+    return (
+      Math.floor(getDate(b).getTime() / 1000) -
+      Math.floor(getDate(a).getTime() / 1000)
     );
+  });
 };
 
 export default getSortedPosts;
